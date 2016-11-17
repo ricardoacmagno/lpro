b8a03803ceb905162171662eb9f94fe91eb5e94f
ricardoacmagno <ricardomagno95@gmail.com>
2016-11-17 11:14:43 +0000

    Add files via upload

---
+++ Client_final/src/LogicClient/MainClass.java
@@ -0,0 +1,14 @@
+package LogicClient;
+
+import GUI.UIinicial;
+
+/**
+ *
+ * @author ricar
+ */
+public class MainClass {
+    public static void main(String[] args) {
+        UIinicial login = new UIinicial();
+        login.setVisible(true);
+    }
+}
