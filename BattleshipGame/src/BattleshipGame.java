248f0979cd731049ca0f4d4ce7b46dfe90f48b36
franciscolfraga <franciscolfraga@gmail.com>
2016-11-18 19:36:29 +0000

    begining of the game

---
+++ BattleshipGame/src/BattleshipGame.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author francisco
+ */
+public class BattleshipGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Board board = new Board();
+        String name1 = "player1";
+        String name2 = "player2";
+        Player player1 = new Player(name1);
+        Player player2 = new Player(name2);
+    }
+    
+}
---
+++ BattleshipGame/src/Board.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author francisco
+ */
+public class Board {
+    char boardtable[][] = new char[10][10];
+    char letters[] = new char[10];
+    int numbers[] = new int[10];
+    public Board(){
+        initBoard(); 
+    }
+    private void initBoard(){
+        System.out.print(" ");
+        for(int c = 0 ; c < 10 ; c++){
+            System.out.print(c);
+            letters[c] = (char) ('A' + c);
+        }
+        System.out.println();
+        for(int c = 0 ; c < 10 ; c++){
+            System.out.println(letters[c]);
+            
+        }
+    }
+    
+}
